b0b561b89648a991d3e0e126dca5e57432624f98
FeiCoding <254356151@qq.com>
2019-03-30 23:34:41 +0000

    finish week 2

--- C++ Programming Design/week2/assignment.cpp
+++ C++ Programming Design/week2/assignment.cpp
@@ -15,36 +15,23 @@ class Student{
     int mark4;
 
     public:
-     void average(){
+     void init(){
+        char comma;
+        getline(cin, name, ',');
+        cin >> age >> comma;
+        getline(cin, id, ',');
+        cin >> mark1 >> comma >> mark2 >> comma >> mark3 >> comma >> mark4;
+     }
+    void average(){
          int average = (mark1 + mark2 + mark3 + mark4) / 4;
          cout << name << ',' << age << ',' << id << ',' << average << endl;
-     }
-     void init(string name_, int age_, string id_, int mark1_, int mark2_, int mark3_, int mark4_){
-         name = name_;
-         age = age_;
-         id = id_;
-         mark1 = mark1_;
-         mark2 = mark2_;
-         mark3 = mark3_;
-         mark4 = mark4_;
      }
 };
 
 int main(){
-    char comma;
-    string name;
-    int age;
-    string id;
-    int mark1;
-    int mark2;
-    int mark3;
-    int mark4;
-    getline(cin, name, ',');
-    cin >> age >> comma;
-    getline(cin, id, ',');
-    cin >> mark1 >> comma >> mark2 >> comma >> mark3 >> comma >> mark4;
+    
     Student s;
-    s.init(name, age, id, mark1, mark2, mark3, mark4);
+    s.init();
     s.average();
     return 0;
 }
