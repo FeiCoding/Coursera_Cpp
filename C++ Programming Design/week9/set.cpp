d7f8b2f99b112a3d125fbb2e8fd731c9ba16dcc3
FeiCoding <254356151@qq.com>
2019-04-22 22:23:57 +0100

    updated with set

--- C++ Programming Design/week9/set.cpp
+++ C++ Programming Design/week9/set.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main(){
-    
+    typedef set<int>::iterator IT;
+    int a[5] = {3, 4, 6, 1, 2};
+    set<int> st(a, a+5);
+    pair<IT, bool> result;
+    result = st.insert(5);
+    if(result.second)
+        cout << "Insert Successfully. " << *result.first << " inserted." << endl;
+
+    result = st.insert(5);
+    if(result.second) cout << *result.first << endl;
+    else cout << *result.first << "Already Existed." << endl;
+    pair<IT, IT> bound = st.equal_range(4);
+    cout << *bound.first << " " << *bound.second << endl;
     return 0;
 }
