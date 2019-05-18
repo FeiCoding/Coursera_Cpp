4d8bdf33dcf31873061e6edf487d0e6c63b9055f
FeiCoding <254356151@qq.com>
2019-05-18 23:44:28 +0100

    update code

--- C++ Programming Design/week10/part5.cpp
+++ C++ Programming Design/week10/part5.cpp
@@ -3,7 +3,15 @@
 #include <algorithm>
 using namespace std;
 // 在此处补充你的代码
-
+struct CMy_add
+{
+	int &sum;
+	CMy_add(int &s) :sum(s) {}
+	void operator()(int val)
+	{
+		sum += (val & 7);
+	}
+};
 
 
 
---
+++ C++ Programming Design/week10/submit_code/p7.cpp
@@ -0,0 +1,23 @@
+// 在此处补充你的代码
+struct Print{
+    void operator()(A& a){
+        cout << a << " ";
+    }
+};
+template <class T, class Func>
+void Show(T init, T end, Func fun){
+    for(;init != end; init++){
+        func(init);
+    }
+}
+
+template <class T>
+struct MyLarge{
+    bool operator()(const T &t1, const T &t2){
+        return t1.get_name() < t2.get_name();
+    }
+};
+
+bool operator <(const A &a1, const A &a2){
+    return a1.get_size() < a2.get_size();
+}
